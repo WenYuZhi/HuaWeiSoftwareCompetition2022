567dcfb7a72af84d1a5c607e0b73b061b9bc0bc5
WenYuZhi <YuanWangNeu.gmail.com>
2022-04-02 16:31:05 +0800

    add commit

--- model.py
+++ model.py
@@ -10,44 +10,44 @@ FILE_PATH = './'+ "results//"
 class Model():
     def __init__(self, data_set, qos_ub) -> None:
         self.qos, self.demand, self.bandwidth, self.site_name = data_set.qos, data_set.demand_values, data_set.bandwidth, data_set.site_name
-        self.n_site, self.n_time, self.n_cust = data_set.n_site, data_set.n_time, data_set.n_cust # n_site 基站数量；n_time:时刻数; n_cust:用户数
-        self.qos_ub = qos_ub # qos的上限
+        self.n_site, self.n_cust = data_set.n_site, data_set.n_cust # n_site 基站数量; n_cust:用户数
+        self.qos_ub, self.n_time = qos_ub,  data_set.n_time # qos的上限; n_time:时刻数
         self.m = gp.Model('flow scheduled model') # 建立优化模型
         self.x = self.m.addVars(self.n_cust, self.n_site, self.n_time, lb=0, ub=GRB.INFINITY,vtype=GRB.INTEGER, name ='x') 
         self.w = self.m.addVars(self.n_site, self.n_time, lb=0, ub=GRB.INFINITY,vtype=GRB.INTEGER, name ='w')
-        self.w_max = self.m.addVars(self.n_site, lb=0, ub=GRB.INFINITY,vtype=GRB.INTEGER, name ='w_max')
+        self.w_max = self.m.addVars(self.n_site, lb=0, ub=GRB.INFINITY,vtype=GRB.INTEGER, name ='w_max') # w_max:代表模型中的 s_hat
         
     def sample_time(self, quantile):
         self.quantile_point = math.ceil(quantile*self.n_time)
-        self.sample_t = random.sample([i for i in range(self.n_time)], self.quantile_point)
+        self.sample_t = random.sample([i for i in range(self.n_time)], self.quantile_point) 
     
     def set_objective(self):
         self.m.setObjective(gp.quicksum(self.w_max[j] for j in range(self.n_site))) #目标函数 对应式(14)
     
     def set_qos_constrs(self):
         self.m.addConstrs((self.x[i,j,t] == 0 for i in range(self.n_cust) for j in range(self.n_site) for t in range(self.n_time) \
-        if self.qos[i,j] >= self.qos_ub), name ='qos')
+        if self.qos[i,j] >= self.qos_ub), name ='qos') # qos约束 对应式(2)
     
     def set_demand_constrs(self):
         self.m.addConstrs((gp.quicksum(self.x[i,j,t] for j in range(self.n_site)) == self.demand[i,t] for i in range(self.n_cust) \
-        for t in range(self.n_time)), name = 'demand')
+        for t in range(self.n_time)), name = 'demand') # 需求约束 对应式(4)
     
     def set_bandwidth_constrs(self):
-        self.m.addConstrs((self.w[j,t] <= self.bandwidth[j] for j in range(self.n_site) for t in range(self.n_time)), name = 'bandwidth')
+        self.m.addConstrs((self.w[j,t] <= self.bandwidth[j] for j in range(self.n_site) for t in range(self.n_time)), name = 'bandwidth') # 边缘节点带宽约束 对应式(5)
     
     def set_link_constrs(self):
         self.m.addConstrs((gp.quicksum(self.x[i,j,t] for i in range(self.n_cust)) == self.w[j,t] for j in range(self.n_site) \
-        for t in range(self.n_time)), name ='link')
+        for t in range(self.n_time)), name ='link') # 中间变量约束 对应式(6)
     
-    def set_max_constrs(self):
-        self.m.addConstrs((self.w_max[j] >= self.w[j,t] for j in range(self.n_site) for t in range(self.n_time) if t in self.sample_t), name = 'max')
+    def set_quantile95_constrs(self):
+        self.m.addConstrs((self.w_max[j] >= self.w[j,t] for j in range(self.n_site) for t in range(self.n_time) if t in self.sample_t), name = 'max') # 95分位数约束 对应式(10-11)
 
     def set_constrs(self):
         self.set_qos_constrs()
         self.set_demand_constrs()
         self.set_link_constrs()
         self.set_bandwidth_constrs()
-        self.set_max_constrs()
+        self.set_quantile95_constrs()
     
     def solve(self, time_limit):
         self.m.Params.TimeLimit = time_limit 
